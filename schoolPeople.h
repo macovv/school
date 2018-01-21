4760e4b60cd7e1767358fa4fc35d91ef49873c18
macovv <szymonwojtala122@gmail.com>
2018-01-21 23:58:39 +0100

    Update schoolPeople.h

--- schoolPeople.h
+++ schoolPeople.h
@@ -27,7 +27,8 @@ class Student : public Human{
     public:
         Student(std::string n, std::string ln, int id) 
                     : Human(n, ln), sid(id) {};
-        Student(std::string n, std::string ln, int id,                          std::vector<std::pair<int,int>> gr) : Human(n, ln), sid(id), grades(gr) {};
+        Student(std::string n, std::string ln, int id, std::vector<std::pair<int,int>> gr)
+                    : Human(n, ln), sid(id), grades(gr) {};
         ~Student() {};
         void addGrade(int p, int mp);
         void addGrade();
@@ -38,7 +39,6 @@ class Student : public Human{
             calculateGPA(sp);
         }
         friend  std::ostream & operator<<(std::ostream & os, const Student & s){
-            
             return os << "Student id: " << s.sid << " and his/her gpa is: " << s.gpa << std::endl;
         }
 
